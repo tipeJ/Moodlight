b81254399734ed8b4209d336ca5cb5d22f558964
tipej <tiitus.jarvinen@gmail.com>
2023-10-14 16:42:39 +0300

    Added gradient pulse to light configs

--- lib/models/light_configs.dart
+++ lib/models/light_configs.dart
@@ -25,3 +25,21 @@ class RainbowLightConfiguration extends LightConfiguration {
     return '{"mode": "rainbow", "wait_ms": $waitMS}';
   }
 }
+
+class GradientPulseConfiguration extends LightConfiguration {
+  final Color color1;
+  final Color color2;
+  final int steps;
+  final int waitMS;
+
+  GradientPulseConfiguration(
+      {required this.color1,
+      required this.color2,
+      required this.steps,
+      required this.waitMS});
+
+  @override
+  String toJson() {
+    return '{"mode": "gradientpulse", "c1": [${color1.red}, ${color1.green}, ${color1.blue}], "c2": [${color2.red}, ${color2.green}, ${color2.blue}], "wait_ms": $waitMS, "steps": $steps}';
+  }
+}
